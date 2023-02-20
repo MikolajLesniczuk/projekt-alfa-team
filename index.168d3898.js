e572878591ffb58c5913d1e278b60db24d030e83
MikolajLesniczuk <119354331+MikolajLesniczuk@users.noreply.github.com>
2023-02-20 15:16:50 +0000

    Deploying to gh-pages from @ MikolajLesniczuk/projekt-alfa-team@ac40e6b011f3ae3194840fca55de73c2adef33bd 🚀

---
+++ index.168d3898.js
@@ -0,0 +1,2 @@
+const rangeInput=document.querySelector('input[type="range"]'),output=document.querySelector(".js-selected-value");rangeInput.addEventListener("input",(e=>output.textContent=e.target.value));
+//# sourceMappingURL=index.168d3898.js.map
